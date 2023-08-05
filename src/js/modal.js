d636b2b63be60f34fea82cc6f0c80687a5912999
SerhiiHolovk <98563452+SerhiiHolovk@users.noreply.github.com>
2023-08-05 20:27:14 +0300

    delete+coment

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,3 +13,31 @@
     document.body.classList.toggle('no-scroll');
   }
 })();
+//api information about books
+/*
+function infoBooks(bookId) {
+  return fetch(`https://books-backend.p.goit.global/books/${bookId}`).then(
+    (resp) => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    }
+  );
+}
+
+function renderPosts(resp) {
+  const markup = cardOfBook
+    .map(({ id, title, body, userId }) => {
+      return `<li>
+          <h2 class="post-title">${title.slice(0, 30)}</h2>
+          <p><b>Post id</b>: ${id}</p>
+          <p><b>Author id</b>: ${userId}</p>
+          <p class="post-body">${body}</p>
+        </li>`;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
+
+*/

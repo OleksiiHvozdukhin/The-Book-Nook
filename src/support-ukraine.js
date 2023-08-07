95577917dfa6960efa9701a2c7a4cfb2e504390c
AlexeyHvozduhin <124602564+AlexeyHvozduhin@users.noreply.github.com>
2023-08-07 15:41:05 +0300

    reanimation
    Gathered together all the sections.

--- src/support-ukraine.js
+++ src/support-ukraine.js
@@ -1,5 +1,7 @@
 const refs = {
   supportUkraine: document.querySelector('.support-ukraine'),
+  allCategoties: document.querySelector('.all_categories'),
+  body: document.body,
   funds: document.querySelector('.funds.support-ukraine-container'),
   slider: document.querySelector('.slider-support-ukraine'),
 };
@@ -9,4 +11,6 @@ refs.slider.addEventListener('click', onSliderClick);
 function onSliderClick() {
   fundsList.classList.toggle('active');
   refs.supportUkraine.classList.toggle('active');
+  if (refs.body.offsetWidth >= 768 && refs.body.offsetWidth <= 1440)
+    refs.allCategoties.classList.toggle('active');
 }

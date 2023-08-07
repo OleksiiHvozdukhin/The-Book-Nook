0cce9eeaf2adc1bdca24a31d594e105e01f401d1
AlexeyHvozduhin <124602564+AlexeyHvozduhin@users.noreply.github.com>
2023-08-07 15:49:28 +0300

    reanimation 2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 // Theme toggle
-const themeSwitcher = document.getElementById("slider");
+const themeSwitcher = document.getElementById('slider');
 const body = document.body;
 const header = document.getElementById('header');
 const logo = document.getElementById('logo');
 const mobileMenuButton = document.querySelector('[data-menu-open]');
 const mobileMenuCloseButton = document.querySelector('[data-menu-close]');
 
-
 function toggleTheme() {
-  body.classList.toggle("dark-theme");
-  if (body.classList.contains("dark-theme")) {
-    localStorage.setItem("theme", "dark");
+  body.classList.toggle('dark-theme');
+  if (body.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'dark');
     setDarkThemeStyles();
   } else {
-    localStorage.setItem("theme", "light");
+    localStorage.setItem('theme', 'light');
     setLightThemeStyles();
   }
 }
@@ -22,9 +21,8 @@ function setDarkThemeStyles() {
   header.style.setProperty('--header-bg-color-light', '#111111');
   header.style.setProperty('--header-border-color-light', '#fff');
   // logo.src = "./images/1-Header/logo_dark.svg";
-  mobileMenuCloseButton.style.visibility = "visible";
+  mobileMenuCloseButton.style.visibility = 'visible';
 }
-
 
 function setLightThemeStyles() {
   header.style.setProperty('--header-bg-color-light', '#f9f9f9');
@@ -32,19 +30,17 @@ function setLightThemeStyles() {
   // logo.src = "./images/1-Header/logo_light.svg";
 }
 
-
-if (localStorage.getItem("theme") === "dark") {
-  body.classList.add("dark-theme");
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-theme');
   themeSwitcher.checked = true;
   setDarkThemeStyles();
 } else {
-  body.classList.remove("dark-theme");
+  body.classList.remove('dark-theme');
   themeSwitcher.checked = false;
   setLightThemeStyles();
 }
 
-themeSwitcher.addEventListener("change", toggleTheme);
-
+themeSwitcher.addEventListener('change', toggleTheme);
 
 // Modal menu
 (() => {
@@ -54,7 +50,7 @@ themeSwitcher.addEventListener("change", toggleTheme);
     menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openMenuBtn.forEach((el) => {
+  refs.openMenuBtn.forEach(el => {
     el.addEventListener('click', toggleMenu);
   });
 
@@ -62,7 +58,7 @@ themeSwitcher.addEventListener("change", toggleTheme);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    refs.openMenuBtn.forEach((el) => el.classList.toggle('is-hidden'));
+    refs.openMenuBtn.forEach(el => el.classList.toggle('is-hidden'));
     refs.closeMenuBtn.classList.toggle('is-hidden');
   }
 })();

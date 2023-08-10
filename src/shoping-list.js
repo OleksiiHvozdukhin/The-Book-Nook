42710ebc305e92bfb39ca52321ddd97bf5dbcb46
AlexeyHvozduhin <124602564+AlexeyHvozduhin@users.noreply.github.com>
2023-08-10 05:29:50 +0300

    Add shoping-list

---
+++ src/shoping-list.js
@@ -0,0 +1,44 @@
+function servicesSelectedBook(idBook) {
+  return fetch(`https://books-backend.p.goit.global/books/${idBook}`).then(
+    resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    }
+  );
+}
+
+if (localStorage.length > 1) {
+  const clearShopList = document.querySelector('.empty-shopping-list-div');
+  const bookList = document.querySelector('.shopping-book-list');
+  for (const key in localStorage) {
+    if (localStorage[key] == 'BOOK')
+      servicesSelectedBook(key).then(
+        ({ _id, book_image, author, title, description, list_name }) => {
+          console.log(_id);
+          bookList.insertAdjacentHTML(
+            'beforeend',
+            `<li class="shoping-book-card">
+      <div class="shoping-book-card-img">
+        <img src="${book_image}" alt="${_id}"width="335" height="485">
+      </div>
+      <div class="shoping-book-card-text">
+        <div class="shoping-book-card-title_and_ganre">
+            <h3 class="shoping-book-card-title">${title}</h3>
+            <h4 class="shoping-book-card-ganre">${list_name}</h4>
+            <p class="shoping-book-card-description">${description}</p>
+        </div>
+        <h4 class="shoping-book-card-author">${author}</h4>
+      </div>
+    </li>`
+          );
+        }
+      );
+  }
+
+  clearShopList.style.display = 'none';
+} else {
+  const clearShopList = document.querySelector('.empty-shopping-list-div');
+  clearShopList.style.display = 'block';
+}
